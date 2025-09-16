644f7cb4d077890bac1e5e612231020d23978749
SumindaBandara <sumindabandarapc@gmail.com>
2025-09-17 01:10:22 +0530

    updated

--- fee-storefront-backend-week-15/fee-storefront-backend-week-15/src/application/dto/categories.js
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const createCategoryDto = z.object({
-  name: z.string(),
-});
--- fee-storefront-backend-week-15/fee-storefront-backend-week-15/src/application/dto/products.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const createProductDto = z.object({
-  categoryId: z.string(),
-  image: z.string(),
-  name: z.string(),
-  price: z.string(),
-  description: z.string(),
-});
